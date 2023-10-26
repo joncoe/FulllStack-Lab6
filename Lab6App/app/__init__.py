279add5144c20fa765bdd5674f300d47578c1293
joncoe <info@joncoe.com>
2023-10-25 20:10:55 -0400

    Add update user

--- Lab6App/app/__init__.py
+++ Lab6App/app/__init__.py
@@ -33,12 +33,14 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
 from . import views, models
 
 
+# List ALL users
 @app.route("/list", methods=["GET"])
 def list_all_users():
     result_set = db.session.query(models.User).all()
     return jsonify(result_set)
 
 
+# Add NEW user
 @app.route("/insert", methods=["POST"])
 def insert_user():
     name = request.form.get("user_name")
@@ -53,5 +55,27 @@ def insert_user():
     db.session.commit()
 
     status_message = "Row with a primary 🔑 of " + email + " has been inserted"
+
+    return jsonify({"📋:": status_message})
+
+
+#  UPDATE User
+@app.route("/update", methods=["POST"])
+def update_user():
+    name = request.form.get("user_name")
+    email = request.form.get("user_email")
+
+    query = db.session.query(models.User)
+    query = query.filter(models.User.email == email)
+    rows_changed = query.update({models.User.name: name})
+
+    print("🥞 Rows changed: ", rows_changed)
+    print("🔎 query", query)
+
+    # flush before commit
+    db.session.flush()
+    db.session.commit()
+
+    status_message = str(rows_changed) + " rows have been affected/changed"
 
     return jsonify({"📋:": status_message})
